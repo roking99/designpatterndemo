86b98856a11851cc9bcd59b4c8fd7522ddb09d8e
luojin57 <luojin1@jd.com>
2017-11-15 18:58:03 +0800

    修改描述文件名称; 提交桥接模式;

---
+++ bridgepattern/src/main/java/com/roking/designpattern/implement/texttype/PdfTextConverter.java
@@ -0,0 +1,14 @@
+package com.roking.designpattern.implement.texttype;
+
+/**
+ * Title:</br>
+ * <p/>
+ * Description: </br>
+ * <p/>
+ * Company: <a href="www.jd.com">京东</a></br>
+ *
+ * @author <a href=mailto:luojin1@jd.com>罗金</a>
+ * @date 2017/11/15.
+ */
+public class PdfTextConverter {
+}
